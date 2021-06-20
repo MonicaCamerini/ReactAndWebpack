7b59e40e6cc1184ac7733c78125adc881a1f10dc
Monica <monica.s.camerini@gmail.com>
2021-06-19 23:22:09 -0300

    feat: first implementations

---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import React from 'react'
+import createReactClass from 'create-react-class'
+
+const App = createReactClass({
+  render: function () {
+    return <div>Aplicação da Mônica</div>
+  }
+})
+
+export default App
